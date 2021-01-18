1249fa9b853d9faf344363769eb57149a195c292
Mari023 <Mari_023@outlook.de>
2021-01-18 16:24:56 +0100

    test if user works properly
    Signed-off-by: Mari023 <Mari_023@outlook.de>

--- app/src/main/java/de/mari_023/android/catscradle/MainActivity.java
+++ app/src/main/java/de/mari_023/android/catscradle/MainActivity.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    final int questions = 2;
+    final int questions = 3;
     Button answerA, answerB, answerC, answerD;
     TextView question;
     Answer correct;
